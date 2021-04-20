aa92ce4adc2baa242ef79aaefcd9e372752b80cb
student <k.saito70329@gamil.com>
2021-04-20 11:57:18 +0900

    [add] 2021/04/20

--- main.go
+++ main.go
@@ -7,4 +7,7 @@ import (
 
 func main() {
 	
+	{
+
+	}
 }
--- main_reference.go
+++ main_reference.go
@@ -8,6 +8,19 @@ import (
 func main() {
 	// 変数の定義 ver 変数名 型
 	var name string
+	// 宣言の省略(型は自動的に決まる)
+	names := サンプル君
+
+	/* 定数の定義(グローバルとして使用する場合は関数外
+	で定義する)*/
+	const PI = 3.14
+	// 自動で1づつ増やす
+	const (
+		Sun int = iota
+		Mon
+		Tue
+	)
+
 	// f.Printf("名前を入力してください") 任意の名前指定したver
 	fmt.Printf("名前を入力してください")
 	fmt.Scan(&name)
