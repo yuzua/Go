3b2aecf68cdf08e1184970bba90ffdcf5658a07a
ゆずき <k.saito70329@gmail.com>
2021-05-02 23:37:33 +0900

    2021/05/02

---
+++ finterfase.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// 入れた値によって勝手に型を変更する(Pythonみたい)
+	 var i interface{}
+	 i = 100
+	 fmt.Printf("%T\n",i) // int
+	 i = 0.5
+	 fmt.Printf("%T\n",i) // float64
+	 i = "hello"
+	 fmt.Printf("%T\n",i) // string
+	 // 型の調べ方 i.(type)
+	 // 使い方
+	switch i.(type) {
+	case string:
+		fmt.Printf("文字列型です")
+	}
+}
