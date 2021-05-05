6123a1a2c66cda7911d47f9d2be29b959c787e22
ゆずき <k.saito70329@gmail.com>
2021-05-05 22:32:00 +0900

    [add] 2021/05/05

--- pointer.go
+++ pointer.go
@@ -5,10 +5,28 @@ import (
 	"fmt"
 )
 
+func byVal(a int){
+	a = a+1
+}
+
+func byRef(a *int){
+	*a = *a +1
+	// aのアドレスを調べるには"%p"を使用
+	fmt.Printf("%p\n", a)
+}
+
 func main() {
 	var a int
 	var p *int
 	a = 100
 	p = &a // "&"はアドレス演算子 "a"のアドレスを調べて,その値を"p"に代入
 	fmt.Printf(*p) // "*"はアドレスの中身 = aの値と同じ
+
+	var n = 1
+	byVal(n)
+	fmt.Println("n=",n)
+
+	n = 1
+	byRef(&n)
+	fmt.Println("n=",n)
 }
